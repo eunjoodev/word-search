7911064740baf9130ae5cca9ccdc7f2d89cbdb7d
eunjoodev <eunjoo.dev@gmail.com>
2024-05-28 03:47:14 +0900

    svelte, vite로 바꾼 후 firebase 배포 준비

---
+++ src/pages/Maker.svelte
@@ -0,0 +1,194 @@
+  <header>
+
+    <div class="header-logo">
+      <img src="assat/Word-Search-Logo.png" alt="Logo" />
+    </div>
+
+    <div class="header-icons">
+      <img src="assat/question.svg" alt="question" />
+      <span>Word Search Maker</span>
+    </div>
+
+  </header>
+
+  <main>
+    <div class="manage-existing">
+      <button class="manage-button">Manege existing puzzles here</button>
+    </div>
+    <form id="maker-form">
+      <div class="form-all">
+        <div class="title">
+          <div class="title-label-box">
+            <label for="title-input">Title</label>
+          </div>
+          <div class="title-input-box">
+            <input
+              type="text"
+              id="title-input"
+              placeholder=" Word Search Game"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="description">
+          <div class="description-label-box">
+            <label for="description-input">Description</label>
+          </div>
+          <div class="description-input-box">
+            <input
+              type="text"
+              id="description-input"
+              placeholder=" Make your own word search game on any topic you like, simply by providing between 10 and 30 words."
+              required
+            />
+          </div>
+        </div>
+        <div class="word-list">
+          <div>
+            <label for="word-list-input">Word list</label>
+          </div>
+          <span class="list-dec"
+            >Between 10 and 30 words. Puzzles are randomly generated using a
+            selection of your words at play time.</span
+          >
+          <div class="word-list-row row-0">
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" APPLE"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" BANANA"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" CHERRY"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" GRAPES"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" GUAVA"
+              required
+            />
+          </div>
+          <div class="word-list-row row-1">
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" KIWI"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" LEMON"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" MANGO"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" ORANGE"
+              required
+            />
+            <input
+              type="text"
+              id="word-list-input"
+              placeholder=" STRAWBERRY"
+              required
+            />
+          </div>
+          <div class="word-list-row row-2">
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+          </div>
+          <div class="word-list-row row-3">
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+          </div>
+          <div class="word-list-row row-4">
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+          </div>
+          <div class="word-list-row row-5">
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+            <input type="text" id="word-list-input" />
+          </div>
+        </div>
+          <a class="submit-button" id="word-submit" href="#/user">
+            START!
+          </a>
+      </div>
+    </form>
+  </main>
+
+  <style>
+
+  header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+  border-bottom: 4px solid black;
+  padding-top: 10px;
+}
+
+.header-logo {
+  position: relative;
+  padding-left: 120px;
+  padding-bottom: 10px;
+}
+
+.header-logo img {
+  width: 300px;
+  height: 70px;
+  position: relative;
+}
+
+.header-icons img {
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+}
+
+.header-icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 25px;
+  padding-right: 120px;
+  font-size: 18px;
+  color: rgb(81, 81, 81);
+}
+  </style>
---
+++ src/pages/User.svelte
@@ -0,0 +1,420 @@
+<script>
+
+</script>
+
+
+
+<div class="back">
+    <form class="back-container">
+        <div class="ready-play">READY TO PLAY 🧩</div>
+        <label class="name-label" for="user-name">이름을 입력해주세요.</label>
+        <input class="name-input" type="text" id="name-input" required />
+        <div class="win-des">
+          <span
+            >게임 성공 시, 꽁꽁 얼어붙은 한강위로 걸어다니는 <br />고양이를 만날
+            수 있습니다. 🤍
+          </span>
+        </div>
+
+          <a class="user-submit" id="user-submit" type="submit" href="#/main">
+            GAME START
+          </a>
+
+        
+      </form>
+    
+      <header>
+        <div class="header-logo">
+          <img src="assat/Word-Search-Logo.png" alt="Logo" />
+        </div>
+    
+        <div class="header-icons">
+          <img src="assat/question.svg" alt="question" />
+          Word Search Maker
+        </div>
+      </header>
+    
+      <div class="time">
+        <h2 id="timer">00:00</h2>
+      </div>
+    
+      <main>
+        <div class="board-block">
+          <div class="board-row row-0">
+            <div class="board-column" data-index="00" data-key="Q">G</div>
+            <div class="board-column" data-index="01" data-key="Q">R</div>
+            <div class="board-column" data-index="02" data-key="Q">A</div>
+            <div class="board-column" data-index="03" data-key="Q">P</div>
+            <div class="board-column" data-index="04" data-key="Q">E</div>
+            <div class="board-column" data-index="05" data-key="Q">S</div>
+            <div class="board-column" data-index="06" data-key="Q">N</div>
+            <div class="board-column" data-index="07" data-key="Q">K</div>
+            <div class="board-column" data-index="08" data-key="Q">H</div>
+            <div class="board-column" data-index="09" data-key="Q">N</div>
+            <div class="board-column" data-index="10" data-key="Q">U</div>
+            <div class="board-column" data-index="11" data-key="Q">O</div>
+          </div>
+          <div class="board-row row-1">
+            <div class="board-column" data-index="12" data-key="Q">A</div>
+            <div class="board-column" data-index="13" data-key="Q">W</div>
+            <div class="board-column" data-index="14" data-key="Q">N</div>
+            <div class="board-column" data-index="15" data-key="Q">T</div>
+            <div class="board-column" data-index="16" data-key="Q">E</div>
+            <div class="board-column" data-index="17" data-key="Q">A</div>
+            <div class="board-column" data-index="18" data-key="Q">I</div>
+            <div class="board-column" data-index="19" data-key="Q">G</div>
+            <div class="board-column" data-index="20" data-key="Q">A</div>
+            <div class="board-column" data-index="21" data-key="Q">B</div>
+            <div class="board-column" data-index="22" data-key="Q">W</div>
+            <div class="board-column" data-index="23" data-key="Q">N</div>
+          </div>
+          <div class="board-row row-2">
+            <div class="board-column" data-index="24" data-key="Q">O</div>
+            <div class="board-column" data-index="25" data-key="Q">A</div>
+            <div class="board-column" data-index="26" data-key="Q">G</div>
+            <div class="board-column" data-index="27" data-key="Q">N</div>
+            <div class="board-column" data-index="28" data-key="Q">O</div>
+            <div class="board-column" data-index="29" data-key="Q">W</div>
+            <div class="board-column" data-index="30" data-key="Q">A</div>
+            <div class="board-column" data-index="31" data-key="Q">M</div>
+            <div class="board-column" data-index="32" data-key="Q">R</div>
+            <div class="board-column" data-index="33" data-key="Q">N</div>
+            <div class="board-column" data-index="34" data-key="Q">A</div>
+            <div class="board-column" data-index="35" data-key="Q">P</div>
+          </div>
+          <div class="board-row row-3">
+            <div class="board-column" data-index="36" data-key="Q">G</div>
+            <div class="board-column" data-index="37" data-key="Q">N</div>
+            <div class="board-column" data-index="38" data-key="Q">C</div>
+            <div class="board-column" data-index="39" data-key="Q">I</div>
+            <div class="board-column" data-index="40" data-key="Q">I</div>
+            <div class="board-column" data-index="41" data-key="Q">S</div>
+            <div class="board-column" data-index="42" data-key="Q">H</div>
+            <div class="board-column" data-index="43" data-key="Q">A</div>
+            <div class="board-column" data-index="44" data-key="Q">I</div>
+            <div class="board-column" data-index="45" data-key="Q">N</div>
+            <div class="board-column" data-index="46" data-key="Q">W</div>
+            <div class="board-column" data-index="47" data-key="Q">N</div>
+          </div>
+          <div class="board-row row-4">
+            <div class="board-column" data-index="00" data-key="Q">N</div>
+            <div class="board-column" data-index="01" data-key="Q">A</div>
+            <div class="board-column" data-index="02" data-key="Q">N</div>
+            <div class="board-column" data-index="03" data-key="Q">E</div>
+            <div class="board-column" data-index="04" data-key="Q">O</div>
+            <div class="board-column" data-index="05" data-key="Q">S</div>
+            <div class="board-column" data-index="06" data-key="Q">E</div>
+            <div class="board-column" data-index="07" data-key="Q">R</div>
+            <div class="board-column" data-index="08" data-key="Q">W</div>
+            <div class="board-column" data-index="09" data-key="Q">R</div>
+            <div class="board-column" data-index="10" data-key="Q">P</div>
+            <div class="board-column" data-index="11" data-key="Q">B</div>
+          </div>
+          <div class="board-row row-5">
+            <div class="board-column" data-index="12" data-key="Q">A</div>
+            <div class="board-column" data-index="13" data-key="Q">P</div>
+            <div class="board-column" data-index="14" data-key="Q">A</div>
+            <div class="board-column" data-index="15" data-key="Q">C</div>
+            <div class="board-column" data-index="16" data-key="Q">H</div>
+            <div class="board-column" data-index="17" data-key="Q">E</div>
+            <div class="board-column" data-index="18" data-key="Q">R</div>
+            <div class="board-column" data-index="19" data-key="Q">R</div>
+            <div class="board-column" data-index="20" data-key="Q">Y</div>
+            <div class="board-column" data-index="21" data-key="Q">E</div>
+            <div class="board-column" data-index="22" data-key="Q">L</div>
+            <div class="board-column" data-index="23" data-key="Q">H</div>
+          </div>
+          <div class="board-row row-6">
+            <div class="board-column" data-index="24" data-key="Q">M</div>
+            <div class="board-column" data-index="25" data-key="Q">P</div>
+            <div class="board-column" data-index="26" data-key="Q">O</div>
+            <div class="board-column" data-index="27" data-key="Q">E</div>
+            <div class="board-column" data-index="28" data-key="Q">W</div>
+            <div class="board-column" data-index="29" data-key="Q">R</div>
+            <div class="board-column" data-index="30" data-key="Q">E</div>
+            <div class="board-column" data-index="31" data-key="Q">L</div>
+            <div class="board-column" data-index="32" data-key="Q">N</div>
+            <div class="board-column" data-index="33" data-key="Q">G</div>
+            <div class="board-column" data-index="34" data-key="Q">P</div>
+            <div class="board-column" data-index="35" data-key="Q">N</div>
+          </div>
+          <div class="board-row row-7">
+            <div class="board-column" data-index="36" data-key="Q">I</div>
+            <div class="board-column" data-index="37" data-key="Q">L</div>
+            <div class="board-column" data-index="38" data-key="Q">S</div>
+            <div class="board-column" data-index="39" data-key="Q">A</div>
+            <div class="board-column" data-index="40" data-key="Q">B</div>
+            <div class="board-column" data-index="41" data-key="Q">N</div>
+            <div class="board-column" data-index="42" data-key="Q">T</div>
+            <div class="board-column" data-index="43" data-key="Q">E</div>
+            <div class="board-column" data-index="44" data-key="Q">E</div>
+            <div class="board-column" data-index="45" data-key="Q">E</div>
+            <div class="board-column" data-index="46" data-key="Q">E</div>
+            <div class="board-column" data-index="47" data-key="Q">O</div>
+          </div>
+          <div class="board-row row-8">
+            <div class="board-column" data-index="00" data-key="Q">K</div>
+            <div class="board-column" data-index="01" data-key="Q">E</div>
+            <div class="board-column" data-index="02" data-key="Q">N</div>
+            <div class="board-column" data-index="03" data-key="Q">N</div>
+            <div class="board-column" data-index="04" data-key="Q">G</div>
+            <div class="board-column" data-index="05" data-key="Q">A</div>
+            <div class="board-column" data-index="06" data-key="Q">G</div>
+            <div class="board-column" data-index="07" data-key="Q">Y</div>
+            <div class="board-column" data-index="08" data-key="Q">A</div>
+            <div class="board-column" data-index="09" data-key="Q">M</div>
+            <div class="board-column" data-index="10" data-key="Q">R</div>
+            <div class="board-column" data-index="11" data-key="Q">E</div>
+          </div>
+          <div class="board-row row-9">
+            <div class="board-column" data-index="12" data-key="Q">R</div>
+            <div class="board-column" data-index="13" data-key="Q">R</div>
+            <div class="board-column" data-index="14" data-key="Q">R</div>
+            <div class="board-column" data-index="15" data-key="Q">E</div>
+            <div class="board-column" data-index="16" data-key="Q">A</div>
+            <div class="board-column" data-index="17" data-key="Q">U</div>
+            <div class="board-column" data-index="18" data-key="Q">N</div>
+            <div class="board-column" data-index="19" data-key="Q">R</div>
+            <div class="board-column" data-index="20" data-key="Q">N</div>
+            <div class="board-column" data-index="21" data-key="Q">A</div>
+            <div class="board-column" data-index="22" data-key="Q">O</div>
+            <div class="board-column" data-index="23" data-key="Q">E</div>
+          </div>
+          <div class="board-row row-10">
+            <div class="board-column" data-index="24" data-key="Q">R</div>
+            <div class="board-column" data-index="25" data-key="Q">E</div>
+            <div class="board-column" data-index="26" data-key="Q">R</div>
+            <div class="board-column" data-index="27" data-key="Q">P</div>
+            <div class="board-column" data-index="28" data-key="Q">A</div>
+            <div class="board-column" data-index="29" data-key="Q">G</div>
+            <div class="board-column" data-index="30" data-key="Q">G</div>
+            <div class="board-column" data-index="31" data-key="Q">A</div>
+            <div class="board-column" data-index="32" data-key="Q">N</div>
+            <div class="board-column" data-index="33" data-key="Q">R</div>
+            <div class="board-column" data-index="34" data-key="Q">G</div>
+            <div class="board-column" data-index="35" data-key="Q">N</div>
+          </div>
+          <div class="board-row row-11">
+            <div class="board-column" data-index="36" data-key="Q">N</div>
+            <div class="board-column" data-index="37" data-key="Q">S</div>
+            <div class="board-column" data-index="38" data-key="Q">E</div>
+            <div class="board-column" data-index="39" data-key="Q">V</div>
+            <div class="board-column" data-index="40" data-key="Q">R</div>
+            <div class="board-column" data-index="41" data-key="Q">L</div>
+            <div class="board-column" data-index="42" data-key="Q">A</div>
+            <div class="board-column" data-index="43" data-key="Q">G</div>
+            <div class="board-column" data-index="44" data-key="Q">N</div>
+            <div class="board-column" data-index="45" data-key="Q">S</div>
+            <div class="board-column" data-index="46" data-key="Q">H</div>
+            <div class="board-column" data-index="47" data-key="Q">R</div>
+          </div>
+          <div class="board-row row-12">
+            <div class="board-column" data-index="24" data-key="Q">E</div>
+            <div class="board-column" data-index="25" data-key="Q">R</div>
+            <div class="board-column" data-index="26" data-key="Q">A</div>
+            <div class="board-column" data-index="27" data-key="Q">N</div>
+            <div class="board-column" data-index="28" data-key="Q">Y</div>
+            <div class="board-column" data-index="29" data-key="Q">N</div>
+            <div class="board-column" data-index="30" data-key="Q">E</div>
+            <div class="board-column" data-index="31" data-key="Q">P</div>
+            <div class="board-column" data-index="32" data-key="Q">E</div>
+            <div class="board-column" data-index="33" data-key="Q">A</div>
+            <div class="board-column" data-index="34" data-key="Q">A</div>
+            <div class="board-column" data-index="35" data-key="Q">B</div>
+          </div>
+          <div class="board-row row-13">
+            <div class="board-column" data-index="36" data-key="Q">I</div>
+            <div class="board-column" data-index="37" data-key="Q">G</div>
+            <div class="board-column" data-index="38" data-key="Q">S</div>
+            <div class="board-column" data-index="39" data-key="Q">T</div>
+            <div class="board-column" data-index="40" data-key="Q">R</div>
+            <div class="board-column" data-index="41" data-key="Q">A</div>
+            <div class="board-column" data-index="42" data-key="Q">W</div>
+            <div class="board-column" data-index="43" data-key="Q">B</div>
+            <div class="board-column" data-index="44" data-key="Q">E</div>
+            <div class="board-column" data-index="45" data-key="Q">R</div>
+            <div class="board-column" data-index="46" data-key="Q">R</div>
+            <div class="board-column" data-index="47" data-key="Q">Y</div>
+          </div>
+        </div>
+        <div class="answer-block">
+          <div class="your-word">🧩 Your Word List</div>
+          <hr />
+          <div>APPLE</div>
+          <div>BANANA</div>
+          <div>CHERRY</div>
+          <div>GRAPES</div>
+          <div>GUAVA</div>
+          <div>KIWI</div>
+          <div>LEMON</div>
+          <div>MANGO</div>
+          <div>ORANGE</div>
+          <div>STRAWBERRY</div>
+        </div>
+      </main>
+</div>
+
+<style>
+
+  header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+  border-bottom: 4px solid black;
+  padding-top: 10px;
+}
+
+.header-logo {
+  position: relative;
+  padding-left: 120px;
+  padding-bottom: 10px;
+}
+
+.header-logo img {
+  width: 300px;
+  height: 70px;
+  position: relative;
+}
+
+.header-icons img {
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+}
+
+.header-icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 25px;
+  padding-right: 120px;
+  font-size: 18px;
+  color: rgb(81, 81, 81);
+}
+
+.back {
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.8);
+}
+
+.time {
+  padding-left: 120px;
+  font-size: 26px;
+  margin-bottom: 12px;
+  font-weight: 600;
+}
+
+main {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: start;
+}
+
+.board-block {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  padding-left: 120px;
+}
+
+.board-row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.board-column {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #d3d6da;
+  width: 45px;
+  height: 40px;
+  font-weight: bold;
+  font-size: 26px;
+  color: rgb(81, 81, 81);
+}
+
+.answer-block {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  margin-left: 40px;
+  margin-top: 20px;
+  font-size: 18px;
+  font-weight: bold;
+  gap: 6px;
+}
+
+.your-word {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.back-container {
+  position: absolute;
+  top: calc(50vh - 170px);
+  left: calc(50vw - 200px);
+  background-color: #418585;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-evenly; */
+  align-items: center;
+  border-radius: 10px;
+  width: 400px;
+  height: 320px;
+  box-shadow: 6px 6px 6px 6px #2e2e2e;
+}
+
+.ready-play {
+  color: #fff;
+  margin-top: 30px;
+  margin-bottom: 26px;
+  font-weight: 800;
+  font-size: 26px;
+}
+
+.name-label {
+  color: #fff;
+  margin-bottom: 10px;
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.name-input {
+  width: 300px;
+  height: 30px;
+  margin-bottom: 14px;
+  border: 3px solid rgb(53, 53, 53);
+}
+
+.win-des {
+  color: #fff;
+  width: 300px;
+  margin-bottom: 30px;
+  font-size: 14px;
+}
+
+#user-submit {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 56px;
+  background-color: #fcec61;
+  border: none;
+  font-size: 20px;
+  font-weight: 800;
+  cursor: pointer;
+  box-shadow: 3px 3px 3px 3px #555555;
+  color: black;
+}
+
+  </style>
